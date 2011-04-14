eab0f255294a7f25a54caa8f059d5aaf47976f47
Aleksander Pohl <apohllo@o2.pl>
2011-04-15 00:59:45 +0200

    - Fix documentation for Cyc::Builder

--- lib/cycr/builder.rb
+++ lib/cycr/builder.rb
@@ -4,9 +4,9 @@ module Cyc
   #
   # This class is used to capture calls to the Cyc client, to allow
   # nested calls, like
-  # 
-  # cyc.with_any_mt do
-  #   comment :Collection
+  #
+  # cyc.with_any_mt do |cyc|
+  #   cyc.comment :Collection
   # end
   class Builder
     def initialize
