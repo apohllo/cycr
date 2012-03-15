5d73284c7a69c038435ade07ad57a4f547f457ed
Aleksander Pohl <apohllo@o2.pl>
2012-03-15 23:37:51 +0100

    Documentation of connection buffer.

--- lib/cyc/connection/buffer.rb
+++ lib/cyc/connection/buffer.rb
@@ -1,42 +1,46 @@
 require "cyc/exception"
-# Author:: Rafal Michalski (mailto:royaltm75@gmail.com)
-# Licence:: MIT/X11 License
-#
-# DataBuffer is chunky text data to server answer assembly class
-# Usage:
-#   b = DataBuffer.new
-#   b << "200 Some response\n" << "300 Some other" << " response\n" << "additional data\n"
-#   b.next_result
-#   => [200, "Some response"]
-#   b.next_result
-#   => [300, "Some other response\nadditional data"]
-#   b.next_result
-#   => nil
-#   b << "Invalid response\n"
-#   b.next_result("meta data")
-#   Cyc::ProtocolError:
-#     Unexpected data from server: "Invalid response", check the submitted query in detail:
-#       "meta data"
-#
-module Cyc
-  module Connection
+
+module Cyc #:nodoc:
+  module Connection #:nodoc:
+    # Author:: Rafal Michalski (mailto:royaltm75@gmail.com)
+    # Licence:: MIT/X11 License
+    #
+    # DataBuffer is chunky text data to server answer assembly class
+    # Usage:
+    #   b = DataBuffer.new
+    #   b << "200 Some response\n" << "300 Some other" << " response\n" << "additional data\n"
+    #   b.next_result
+    #   => [200, "Some response"]
+    #   b.next_result
+    #   => [300, "Some other response\nadditional data"]
+    #   b.next_result
+    #   => nil
+    #   b << "Invalid response\n"
+    #   b.next_result("meta data")
+    #   Cyc::ProtocolError:
+    #     Unexpected data from server: "Invalid response", check the submitted query in detail:
+    #       "meta data"
+    #
     class DataBuffer
       EOL = "\n"
       RESULT_MATCH = /^(\d\d\d) (.*)$/m
 
+      # Initializes an empty buffer.
       def initialize
         @buffer = ""
       end
-      
+
+      # Appends +data+ to the buffer.
       def <<(data)
         @buffer << data
       end
 
+      # Returns the next complete result stored in the buffer.
       def next_result(org_message=nil)
         res_end = 0
         size = @buffer.length
         while res_end = @buffer.index(EOL, res_end)
-          res_end+= 1
+          res_end += 1
           if res_end == size || RESULT_MATCH === @buffer[res_end..-1]
             result = @buffer.slice!(0, res_end).chomp EOL
             if RESULT_MATCH === result
@@ -48,11 +52,13 @@ module Cyc
           end
         end
       end
-      
+
+      # String representation of the buffer.
       def to_s
         @buffer
       end
 
+      # Clears the buffer contents.
       def discard!
         @buffer.clear
       end
